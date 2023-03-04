69dfb6ba8ac0d0436f355ab79931b38c911bc4c5
Bazylek12 <a.kosmowski7@gmail.com>
2023-03-04 19:02:15 +0100

    feat(): login component created

---
+++ ng-material-starter-kit-x-main/src/app/components/login/login.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  imports: [],
+  declarations: [LoginComponent],
+  providers: [],
+  exports: [LoginComponent]
+})
+export class LoginComponentModule {
+}
