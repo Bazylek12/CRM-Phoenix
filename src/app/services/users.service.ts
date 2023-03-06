a5837f23a7056b99225b537d4f606fbb661a5c38
Bazylek12 <a.kosmowski7@gmail.com>
2023-03-06 21:26:05 +0100

    feat(): user details and add bio method implemented

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {environment} from "../../environments/environment";
+import { Observable, shareReplay } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { UserResponse } from '../responses/user.response';
+import { ApiResponse } from '../responses/api.response';
+import { environment } from '../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class UsersService {
   constructor(private _httpClient: HttpClient) {
   }
 
-  getUserData(): Observable<void> {
-    return this._httpClient.get<void>(`${environment.apiUrl}/auth/me`);
+  getUserData(): Observable<UserResponse> {
+    return this.userDetails$
+  }
+
+  private userDetails$: Observable<UserResponse> = this._httpClient.get<ApiResponse<UserResponse>>(`${environment.apiUrl}/auth/me`).pipe(
+    map(response => response.data),
+    shareReplay(1)
+  );
+
+  addBio(content: string): Observable<void> {
+    return this._httpClient.post<void>(`${environment.apiUrl}/auth/add-bio`, {
+        data: {
+            content: content
+        }
+    });
   }
 }
