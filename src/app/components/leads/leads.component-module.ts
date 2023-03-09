870f36fcd3716b7d98349283e95af579e755e237
Bazylek12 <a.kosmowski7@gmail.com>
2023-03-09 22:46:24 +0100

    feat(): leads list implemented

--- src/app/components/leads/leads.component-module.ts
+++ src/app/components/leads/leads.component-module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { LeadsComponent } from './leads.component';
 import {IconsModule} from "../../icons/icons.module";
-import {AsyncPipe, NgIf} from "@angular/common";
+import {AsyncPipe, JsonPipe, NgFor, NgIf} from "@angular/common";
+import { CommonModule } from '@angular/common';
+import {RouterLinkWithHref} from "@angular/router";
 
 @NgModule({
-  imports: [IconsModule, NgIf, AsyncPipe],
+    imports: [IconsModule, NgIf, AsyncPipe, JsonPipe, NgFor, CommonModule, RouterLinkWithHref],
   declarations: [LeadsComponent],
   providers: [],
   exports: [LeadsComponent]
--- src/app/components/leads/leads.component.ts
+++ src/app/components/leads/leads.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, shareReplay } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 import { UserResponse } from '../../responses/user.response';
+import { LeadModel } from '../../models/lead.model';
+import { ActivityModel } from '../../models/activity.model';
 import { UsersService } from '../../services/users.service';
-import {AuthService} from "../../services/auth.service";
-import {Router} from "@angular/router";
+import { AuthService } from '../../services/auth.service';
+import { LeadsService } from '../../services/leads.service';
 
 @Component({
   selector: 'app-leads',
@@ -13,11 +17,50 @@ import {Router} from "@angular/router";
 })
 export class LeadsComponent {
   readonly userDetails$: Observable<UserResponse> = this._usersService.getUserData();
+  readonly getAllLeads$: Observable<LeadModel[]> = this._leadsService.getLeads();
+  readonly getAllActivities$: Observable<ActivityModel[]> = this._leadsService.getActivities()
+  private _filterModalSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public filterModal$: Observable<boolean> = this._filterModalSubject.asObservable();
 
-  constructor(private _usersService: UsersService, private _authService: AuthService, private _router: Router) {
+  constructor(private _usersService: UsersService, private _authService: AuthService, private _router: Router, private _leadsService: LeadsService) {
   }
+
+  readonly LeadsList$: Observable<LeadModel[]> = combineLatest([
+    this._leadsService.getLeads().pipe(shareReplay(1)),
+    this._leadsService.getActivities().pipe(shareReplay(1))
+  ]).pipe(
+    map(([leads, activities]) => this._mapLeadQuery(leads, activities))
+  )
+
   logout(): void {
     this._authService.logout();
     this._router.navigate(['logged-out'])
+  }
+
+  private _mapLeadQuery(leads: LeadModel[], activities: ActivityModel[]): LeadModel[] {
+    const activitiesTagMap = activities.reduce((acc, curr) => (
+      { ...acc, [curr.id]: curr.name }
+    ), {} as Record<string, string>);
+
+    return leads.map(lead => ({
+      name: lead.name,
+      annualRevenue: lead.annualRevenue,
+      hiring: lead.hiring,
+      companySize: lead.companySize,
+      industry: lead.industry,
+      linkedinLink: lead.linkedinLink,
+      location: lead.location,
+      websiteLink: lead.websiteLink,
+      activityIds: lead.activityIds.map(id => activitiesTagMap[id])
+    }))
+  }
+
+  public showFilterModal(): void {
+    this._filterModalSubject.next(true);
+    console.log(this._filterModalSubject.value)
+  }
+
+  public hideFilterModal(): void {
+    this._filterModalSubject.next(false);
   }
 }
