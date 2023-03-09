22c8c3b496b0e151d813f97825e97086000b607b
Bazylek12 <a.kosmowski7@gmail.com>
2023-03-09 22:46:54 +0100

    feat(): leads service implemented

---
+++ src/app/services/leads.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environment} from "../../environments/environment";
+import {LeadModel} from "../models/lead.model";
+import {map, tap} from "rxjs/operators";
+import {ApiResponse} from "../responses/api.response";
+import {ActivityModel} from "../models/activity.model";
+
+@Injectable({ providedIn: 'root' })
+export class LeadsService {
+  constructor(private _httpClient: HttpClient) {
+  }
+
+  getLeads(): Observable<LeadModel[]> {
+    return this._httpClient.get<ApiResponse<LeadModel[]>>(`${environment.apiUrl}/leads`).pipe(
+        map(response => response.data),
+    );
+  }
+
+  getActivities(): Observable<ActivityModel[]> {
+    return this._httpClient.get<ApiResponse<ActivityModel[]>>(`${environment.apiUrl}/leads/activities`).pipe(
+        map(response => response.data),
+    );
+  }
+}
