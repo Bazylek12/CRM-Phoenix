b69bc78aceee96466d8cdd4b76618d005d645d12
Bazylek12 <a.kosmowski7@gmail.com>
2023-03-12 00:51:16 +0100

    feat(): refresh token interceptor implemented

---
+++ src/app/interceptors/refresh-token.interceptor.ts
@@ -0,0 +1,44 @@
+import {Injectable} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import {catchError, Observable, switchMap, throwError} from 'rxjs';
+import {AuthService} from "../services/auth.service";
+@Injectable()
+export class RefreshTokenInterceptor implements HttpInterceptor {
+
+  constructor(private _authService: AuthService) {
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    const refreshToken = localStorage.getItem('refreshToken');
+    const blackList = ['/auth/login']
+    if (blackList.find((url) => request.url.endsWith(url)) !== undefined) {
+      return next.handle(request)
+    } else {
+      return next.handle(request).pipe(
+        catchError((e: HttpErrorResponse) => {
+          if (e.status === 403 && e.error.message === 'Token is invalid') {
+            return this._authService.refreshToken(refreshToken!).pipe(
+              switchMap((newTok) => {
+                const transformedReq = request.clone({
+                  headers: request.headers.set(
+                    'Authorization',
+                    `bearer ${newTok.accessToken}`
+                  ),
+                });
+                return next.handle(transformedReq)
+              })
+            )
+          }
+          return throwError(() => e);
+        })
+      )
+    }
+
+  }
+}
