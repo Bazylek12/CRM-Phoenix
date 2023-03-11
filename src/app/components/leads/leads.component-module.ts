c4d5157f24b1676644e42f602c4bf68e53862926
Bazylek12 <a.kosmowski7@gmail.com>
2023-03-11 20:38:59 +0100

    feat(): routerLink to create-lead route added

--- src/app/components/leads/leads.component-module.ts
+++ src/app/components/leads/leads.component-module.ts
@@ -3,11 +3,11 @@ import { LeadsComponent } from './leads.component';
 import {IconsModule} from "../../icons/icons.module";
 import {AsyncPipe, JsonPipe, NgFor, NgIf} from "@angular/common";
 import { CommonModule } from '@angular/common';
-import {RouterLinkWithHref} from "@angular/router";
+import {RouterLink, RouterLinkWithHref} from "@angular/router";
 import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-    imports: [IconsModule, NgIf, AsyncPipe, JsonPipe, NgFor, CommonModule, RouterLinkWithHref, ReactiveFormsModule],
+  imports: [IconsModule, NgIf, AsyncPipe, JsonPipe, NgFor, CommonModule, RouterLinkWithHref, ReactiveFormsModule, RouterLink],
   declarations: [LeadsComponent],
   providers: [],
   exports: [LeadsComponent]
