52d61822f232ba19720c94ef5ad961ceb542593b
Bazylek12 <a.kosmowski7@gmail.com>
2023-03-12 00:37:34 +0100

    feat(): routerLink to leads from create route added

--- src/app/components/create-lead/create-lead.component-module.ts
+++ src/app/components/create-lead/create-lead.component-module.ts
@@ -3,9 +3,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { CreateLeadComponent } from './create-lead.component';
 import {AsyncPipe, CommonModule, NgClass, NgIf} from "@angular/common";
 import {FeatherModule} from "angular-feather";
+import {RouterLink} from "@angular/router";
 
 @NgModule({
-  imports: [ReactiveFormsModule, NgClass, AsyncPipe, NgIf, FeatherModule, CommonModule],
+  imports: [ReactiveFormsModule, NgClass, AsyncPipe, NgIf, FeatherModule, CommonModule, RouterLink],
   declarations: [CreateLeadComponent],
   providers: [],
   exports: [CreateLeadComponent]
