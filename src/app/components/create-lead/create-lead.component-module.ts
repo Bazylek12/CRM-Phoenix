1a4dd59e0262d56af3eb6281c982aebc7adf7e2a
Bazylek12 <a.kosmowski7@gmail.com>
2023-03-11 20:36:24 +0100

    feat(): create lead form and method implemented

--- src/app/components/create-lead/create-lead.component-module.ts
+++ src/app/components/create-lead/create-lead.component-module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CreateLeadComponent } from './create-lead.component';
+import {AsyncPipe, CommonModule, NgClass, NgIf} from "@angular/common";
+import {FeatherModule} from "angular-feather";
 
 @NgModule({
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass, AsyncPipe, NgIf, FeatherModule, CommonModule],
   declarations: [CreateLeadComponent],
   providers: [],
   exports: [CreateLeadComponent]
--- src/app/components/create-lead/create-lead.component.ts
+++ src/app/components/create-lead/create-lead.component.ts
@@ -1,5 +1,13 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import {BehaviorSubject, Observable, of, shareReplay} from 'rxjs';
+import { take, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { ActivityModel } from '../../models/activity.model';
+import { UsersService } from '../../services/users.service';
+import { AuthService } from '../../services/auth.service';
+import { LeadsService } from '../../services/leads.service';
+import { UserResponse } from '../../responses/user.response';
 
 @Component({
   selector: 'app-create-lead',
@@ -8,12 +16,27 @@ import { FormControl, FormGroup } from '@angular/forms';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CreateLeadComponent {
+  constructor(private _usersService: UsersService,
+              private _authService: AuthService,
+              private _router: Router,
+              private _leadsService: LeadsService) {
+  }
+
+  readonly activitiesList$: Observable<ActivityModel[]> = this._leadsService.getActivities().pipe(
+    shareReplay(1),
+    tap(data => this.addActivitiesControls(data))
+  );
+
+  readonly userDetails$: Observable<UserResponse> = this._usersService.getUserData();
+  readonly statusList$: Observable<string[]> = of(['Preliminaries', 'Investigation', 'Demonstrating Capabilities', 'Obtaining Commitment'])
+  private _userMenuSubject: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  public userMenu$: Observable<boolean> = this._userMenuSubject.asObservable();
   readonly leadInfoForm: FormGroup = new FormGroup({
     companyName: new FormControl(),
     websiteLink: new FormControl(),
     linkedinLink: new FormControl(),
     location: new FormControl(),
-    industrial: new FormControl(),
+    industry: new FormControl(),
     annualRevenue: new FormControl()
   });
   readonly activitiesForm: FormGroup = new FormGroup({});
@@ -33,5 +56,55 @@ export class CreateLeadComponent {
     companySize: this.sizeForm,
     hiring: this.hiringForm,
     status: new FormControl(),
-    notes: new FormControl() });
-}
+    notes: new FormControl(),
+  });
+
+  public toggleUserMenu(): void {
+    this.userMenu$.pipe(
+      take(1),
+      tap(status => {
+        this._userMenuSubject.next(!status)
+      })
+    ).subscribe()
+  }
+
+  logout(): void {
+    this._authService.logout();
+    this._router.navigate(['logged-out'])
+  }
+
+  addActivitiesControls(activityList: ActivityModel[]): void {
+    activityList.forEach((activity) => this.activitiesForm.addControl(activity.id, new FormControl(false)));
+  }
+
+  onCreateLeadFormSubmitted(createLeadForm: FormGroup): void {
+    if (!this.createLeadForm.valid) return;
+      this._leadsService
+        .createLead({
+          name: this.leadInfoForm.value.companyName,
+          websiteLink: this.leadInfoForm.value.websiteLink,
+          linkedinLink: this.leadInfoForm.value.linkedinLink,
+          location: this.leadInfoForm.value.location,
+          industry: this.leadInfoForm.value.industry,
+          annualRevenue: +this.leadInfoForm.value.annualRevenue,
+          activityIds: Object.keys(this.activitiesForm.value).reduce((a: string[], c: string) => {
+            if (this.activitiesForm.value[c]) {
+              return [...a, c];
+            } else {
+              return a;
+            }
+          }, []),
+          companySize: {
+            total: this.sizeForm.value.total,
+            dev: this.sizeForm.value.dev,
+            fe: this.sizeForm.value.fe,
+          },
+          hiring: {
+            active: this.hiringForm.value.active,
+            junior: this.hiringForm.value.junior,
+            talentProgram: this.hiringForm.value.talentProgram
+          }
+        }).subscribe()
+    }
+  }
+
