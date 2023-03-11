171f7ecdbee1fc450a95d6e5df0ed497f56eac58
Bazylek12 <a.kosmowski7@gmail.com>
2023-03-11 18:38:46 +0100

    feat(): create lead component created

---
+++ src/app/components/create-lead/create-lead.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CreateLeadComponent } from './create-lead.component';
+
+@NgModule({
+  imports: [],
+  declarations: [CreateLeadComponent],
+  providers: [],
+  exports: [CreateLeadComponent]
+})
+export class CreateLeadComponentModule {
+}
